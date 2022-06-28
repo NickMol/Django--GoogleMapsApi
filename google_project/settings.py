e7575f1f06123b123b9ac247c66709b350477367
NickMol <nickmol96@hotmail.com>
2022-06-28 21:21:50 +0200

    API KEY MASKED

--- google_project/settings.py
+++ google_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/google/static/'
 
-GOOGLE_API_KEY = 'AIzaSyCvOxxVQwml1uBfGy3tYZDgB7Zy2emiKjw'
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 BASE_COUNTRY = 'NL'
 
 # Default primary key field type
